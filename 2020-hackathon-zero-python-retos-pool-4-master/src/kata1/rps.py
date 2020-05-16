1f6ffa5431b716562ad28f39d483a3ecbc84aaae
paquitto99 <frajavmarmol@gmail.com>
2020-05-16 22:35:52 +0200

    Piedra Papeo o Tijera ACABADO ¡¡¡

--- 2020-hackathon-zero-python-retos-pool-4-master/src/kata1/rps.py
+++ 2020-hackathon-zero-python-retos-pool-4-master/src/kata1/rps.py
@@ -8,22 +8,35 @@ options = ["Piedra", "Papel", "Tijeras"]
 #'Perdiste!'
 
 def quienGana(player, ai):
-    winner = "paquito siempre...."
+    if player == ai:
+        winner = "Empate!"
+        control = 1
+    elif player > ai:
+        winner = "Ganaste!"
+        control = 2
+    else:
+        winner = "Perdiste!"
+        control = 3
+    
+    if ai == 0 and player == 2:
+        winner = "Perdiste!"
+        control = 4
+     
     return winner
 
 # Entry Point
 def Game():
-    #
-    #
+    ai = randint(0,2)
     
-    #
-    #
-    
-    winner = quienGana(player, ai)
-
+        
+    playerName = input("Introduce tu nombre PLAYER ONE.:")
+    player = int(input("Elige 1.- PIEDRA, 2.- PAPEL o 3.- TIJERAS.:"))
+    print("Perfecto " + playerName +  " has elegido " + str(options[player-1]) + ", ahora veremos si le ganas a nuestra IA...")
+    x = input("Pulsa una INTRO para jugartela . . . ")
+    print ("La IA ha elegido " + options[ai] + " así pues... ")
+    winner = quienGana(player-1, ai)
     print(winner)
 
-playerOne = input("Introduce tu nombre PLAYER ONE.:")
-hand = input("Elige PIEDRA, PAPEL o TIJERAS.:")
-a = quienGana(playerOne,3)
-print(options[2])
+#  invoco el juego
+a =  Game()
+ 
