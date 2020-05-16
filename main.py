bb9cf8ad2f319ddc6c8d97bc839dcff06a4cbdc2
paquitto99 <frajavmarmol@gmail.com>
2020-05-16 10:15:34 +0200

    primer dia del hackathon

---
+++ main.py
@@ -0,0 +1,19 @@
+print ('Hola mundo¡¡¡')
+
+'''
+Esto es un comentario en bloque 
+con varias lineas....
+
+'''
+
+# Esto es un comentario de una sola linea...
+
+dato = input('Introduce tu edad.:')
+print (dato)
+
+if (dato > 18):
+    print ('Eres mayor de edad')
+else:
+    print ('Eres menor')
+
+
